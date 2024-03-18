30af5c68bda6924c9eb8a7dbe0a52d29d8c324a9
MBATheGamer <mbathegamer@gmail.com>
2024-03-18 06:39:55 +0100

    Vanilla CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import { useState } from "react";
-import Alert from "./components/Alert";
-import Button from "./components/Button";
+import ListGroup from "./components/ListGroup";
 
 export default function App() {
-  const [alertVisible, setAlertVisible] = useState(false);
+  const items = ["New York", "Los Angeles", "San Francisco"];
 
   return (
     <div>
-      {alertVisible && (
-        <Alert onClose={() => setAlertVisible(false)}>Hello, World!</Alert>
-      )}
-      <Button onClick={() => setAlertVisible(true)}>Show Alert</Button>
+      <ListGroup heading="Miami" items={items} onSelectItem={() => {}} />
     </div>
   );
 }
