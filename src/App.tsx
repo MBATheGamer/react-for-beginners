a8a74b394405bb6fb5fd6347548a634fdae0f054
MBATheGamer <mbathegamer@gmail.com>
2024-03-19 18:02:28 +0100

    Understanding the State Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import Like from "./components/Like";
+import { useState } from "react";
+import Button from "./components/Button";
 
 export default function App() {
+  // [false, true]
+  const [isVisible, setVisibility] = useState(false);
+  const [isApproved, setApproved] = useState(true);
+  // reset to 0
+  let count = 0;
+
+  const handleClick = () => {
+    setVisibility(true);
+    count++;
+    console.log(isVisible); // false
+    console.log(count); // 1
+  };
+
   return (
     <div>
-      <Like onClick={() => console.log("clicked")} />
+      <Button onClick={handleClick}>Show</Button>
     </div>
   );
 }
