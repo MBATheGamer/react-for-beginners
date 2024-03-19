7781a57977b6f71c64ee2057677a6f34f85bd168
MBATheGamer <mbathegamer@gmail.com>
2024-03-19 18:08:48 +0100

    Choosing the State Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
 import { useState } from "react";
-import Button from "./components/Button";
+
+// Avoid redundant state variables.
+// Group related variables inside an object.
+// Avoid deeply nested structures.
 
 export default function App() {
-  // [false, true]
-  const [isVisible, setVisibility] = useState(false);
-  const [isApproved, setApproved] = useState(true);
-  // reset to 0
-  let count = 0;
+  // const [firstName, setFirstName] = useState("");
+  // const [lastName, setLastName] = useState("");
+  const [person, setPerson] = useState({
+    firstName: "",
+    lastName: "",
+  });
 
-  const handleClick = () => {
-    setVisibility(true);
-    count++;
-    console.log(isVisible); // false
-    console.log(count); // 1
-  };
+  const [isLoading, setLoading] = useState(false);
 
   return (
     <div>
-      <Button onClick={handleClick}>Show</Button>
+      {person.firstName} {person.lastName}
     </div>
   );
 }
