809721b2c2c56332912a8fffbd67e61c295cf390
MBATheGamer <mbathegamer@gmail.com>
2024-03-20 18:18:23 +0100

    Updating State 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
 import { useState } from "react";
 
 export default function App() {
-  const [pizza, setPizza] = useState({
-    name: "Spicy Pepperoni",
-    toppings: ["Mushroom"],
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "Product 1", quantity: 1 },
+      { id: 2, title: "Product 2", quantity: 1 },
+    ],
   });
 
   const handleClick = () => {
-    setPizza({ ...pizza, toppings: [...pizza.toppings, "Cheese"] });
+    setCart({
+      ...cart,
+      items: cart.items.map(item =>
+        item.id === 1 ? { ...item, quantity: 2 } : item
+      ),
+    });
   };
 
   return (
     <div>
-      <p>{pizza.name}</p>
+      <p>Cart</p>
       <ul>
-        {pizza.toppings.map(topping => (
-          <li key={topping}>{topping}</li>
+        {cart.items.map(item => (
+          <li key={item.id}>
+            {item.title} {item.quantity}
+          </li>
         ))}
       </ul>
-      <button onClick={handleClick}>Add topping</button>
+      <button onClick={handleClick}>Add Quantity</button>
     </div>
   );
 }
