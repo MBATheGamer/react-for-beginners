d36d4532867c43106ecc6833e2501454145051ce
MBATheGamer <mbathegamer@gmail.com>
2024-03-20 18:39:30 +0100

    Building an Expandable Text Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,14 @@
-import { useState } from "react";
+import ExpandableText from "./components/ExpandableText";
 
 export default function App() {
-  const [cart, setCart] = useState({
-    discount: 0.1,
-    items: [
-      { id: 1, title: "Product 1", quantity: 1 },
-      { id: 2, title: "Product 2", quantity: 1 },
-    ],
-  });
-
-  const handleClick = () => {
-    setCart({
-      ...cart,
-      items: cart.items.map(item =>
-        item.id === 1 ? { ...item, quantity: 2 } : item
-      ),
-    });
-  };
-
   return (
     <div>
-      <p>Cart</p>
-      <ul>
-        {cart.items.map(item => (
-          <li key={item.id}>
-            {item.title} {item.quantity}
-          </li>
-        ))}
-      </ul>
-      <button onClick={handleClick}>Add Quantity</button>
+      <ExpandableText>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto
+        obcaecati debitis soluta repudiandae dolor est in veritatis tempora
+        provident molestias quae amet ea autem aut minus, delectus similique,
+        cumque sint!
+      </ExpandableText>
     </div>
   );
 }
