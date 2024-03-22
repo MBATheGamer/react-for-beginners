88ef692076be8d095616ec14d4f6be916f6faa83
MBATheGamer <mbathegamer@gmail.com>
2024-03-22 07:42:47 +0100

    Building a Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 export default function App() {
   return (
     <div>
-      <ExpandableText>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto
-        obcaecati debitis soluta repudiandae dolor est in veritatis tempora
-        provident molestias quae amet ea autem aut minus, delectus similique,
-        cumque sint!
-      </ExpandableText>
+      <Form />
     </div>
   );
 }
---
+++ src/components/Form.tsx
@@ -0,0 +1,21 @@
+export default function Form() {
+  return (
+    <form>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Name:
+        </label>
+        <input type="text" id="name" className="form-control" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="age" className="form-label">
+          Age
+        </label>
+        <input type="number" id="age" className="form-control" />
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
+}
