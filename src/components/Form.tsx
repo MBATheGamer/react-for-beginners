b0cef1f86c392cd4d042d38745a80a475a3ab9bc
MBATheGamer <mbathegamer@gmail.com>
2024-03-23 20:36:57 +0100

    Schema-Based Validation with Zod

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,16 +1,22 @@
 import { FieldValues, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-type FormData = {
-  name: string;
-  age: number;
-};
+const schema = z.object({
+  name: z.string().min(3, { message: "Name must be at least 3 characters." }),
+  age: z
+    .number({ invalid_type_error: "Age field is required." })
+    .min(18, { message: "Age must be at least 18." }),
+});
+
+type FormData = z.infer<typeof schema>;
 
 export default function Form() {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormData>();
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
 
   const onSubmit = (data: FieldValues) => console.log(data);
 
@@ -24,14 +30,9 @@ export default function Form() {
           type="text"
           id="name"
           className="form-control"
-          {...register("name", { required: true, minLength: 3 })}
+          {...register("name")}
         />
-        {errors.name?.type === "required" && (
-          <p className="text-danger">The name field is required.</p>
-        )}
-        {errors.name?.type === "minLength" && (
-          <p className="text-danger">The name must be at least 3 characters.</p>
-        )}
+        {errors.name && <p className="text-danger">{errors.name.message}</p>}
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
@@ -41,8 +42,9 @@ export default function Form() {
           type="number"
           id="age"
           className="form-control"
-          {...register("age", { required: true })}
+          {...register("age", { valueAsNumber: true })}
         />
+        {errors.age && <p className="text-danger">{errors.age.message}</p>}
       </div>
       <button type="submit" className="btn btn-primary">
         Submit
