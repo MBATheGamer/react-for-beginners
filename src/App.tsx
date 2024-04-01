8ccbacabe3a924083bceaf4336915d2da9f071ab
MBATheGamer <mbathegamer@gmail.com>
2024-04-01 16:51:14 +0100

    Fetching Data

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
-import { useEffect } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-const connect = () => console.log("Connecting...");
-const disconnect = () => console.log("Disconnecting...");
+type User = {
+  id: number;
+  name: string;
+};
 
 export default function App() {
+  const [users, setUsers] = useState<User[]>([]);
+
   useEffect(() => {
-    connect();
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then(response => setUsers(response.data));
+  }, []);
 
-    return () => disconnect();
-  });
-
-  return <div></div>;
+  return (
+    <ul>
+      {users.map(user => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
+  );
 }
